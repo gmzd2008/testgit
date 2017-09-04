0d697516e839c5d7d428e54d7806acdee6eeda20
gmzd2008 <258732809@qq.com>
2017-09-04 17:19:00 +0800

    同一个文件先主 后支修改 提交后合并到主

--- index.php
+++ index.php
@@ -3,4 +3,4 @@ header("Content-type:text/html;charset=utf-8");
 echo  "Hello world!";
 ?>
 <h2>修复404bug后，恢复储藏现场记录并提交</h2>
-<h3>临时fenzhi2工作中，突然要回主分支master 修改一下后提交</h3>
+<h3>临时fenzhi2工作中，突然要回主分支master 修改一下后提交。。。</h3>
